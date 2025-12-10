a82d0f3808b7cfa77a2109d990c671f3a823a2ac
SunniM <sunnismagan@gmail.com>
2025-12-10 16:40:13 -0600

    feat(week6): Complete parameterized tests exercise

---
+++ week6/monday/exercises/parameterized_calculator/src/test/java/com/revature/CalculatorParameterizedTest.java
@@ -0,0 +1,64 @@
+package com.revature;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CalculatorParameterizedTest {
+
+    Calculator calculator = null;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new Calculator();
+    }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+        "1, 2, 3",
+        "2, 2, 4",
+        "3, 2, 5",
+        "4, 4, 8",
+        "5, 5, 10"
+
+    })
+    public void testAdd(int a, int b, int expectedResult) {
+        Assertions.assertEquals(calculator.add(a, b), expectedResult);
+    }
+
+    @ParameterizedTest(name = "Adding {0} and {1} is {2}")
+    @CsvFileSource(resources = "/testData.csv", numLinesToSkip = 1)
+    public void testAddAgain(int a, int b, int expectedResult) {
+        Assertions.assertEquals(calculator.add(a, b), expectedResult);
+    }
+
+    @ParameterizedTest(name = "Adding {0} and {1} is {2} using method")
+    @MethodSource("testDataMethod")
+    public void testAddThirdTime(int a, int b, int expectedResult) {
+        Assertions.assertEquals(calculator.add(a, b), expectedResult);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 100, 0, -2})
+    void isEven_evenNumbers_returnsTrue(int number) {
+        assertTrue(calculator.isEven(number));
+    }
+
+    static Stream<Arguments> testDataMethod() {
+        return Stream.of(
+                Arguments.of(1, 2, 3),
+                Arguments.of(2, 4, 6),
+                Arguments.of(3, -6, -3),
+                Arguments.of(4, 8, 12)
+        );
+    }
+
+}
