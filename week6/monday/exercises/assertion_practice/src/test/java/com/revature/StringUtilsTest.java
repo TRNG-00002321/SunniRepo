eded557882d06f24f101ed8a7a3ce319a5cc5956
SunniM <sunnismagan@gmail.com>
2025-12-10 15:30:53 -0600

    feat(week6): Complete assertion practice exercise

--- week6/monday/exercises/assertion_practice/src/test/java/com/revature/StringUtilsTest.java
+++ week6/monday/exercises/assertion_practice/src/test/java/com/revature/StringUtilsTest.java
@@ -1,9 +1,7 @@
-package com.revature.string;
+package com.revature;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import com.revature.user.model.User;
 
 public class StringUtilsTest {
 
